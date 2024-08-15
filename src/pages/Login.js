358fdcc32ee0460ed858f2cb64328822d215b38c
Awo-Nimi-04 <lawogbemila@gmail.com>
2024-08-15 12:52:05 -0500

    collecting input from form

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useRef } from "react";
 
 const LoginPage = () => {
+  const usernameRef = useRef();
+  const passwordRef = useRef();
+
   const changeHandler = () => {};
 
   const touchHandler = () => {};
+
+  // The data from the form is collected here
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const username = usernameRef.current.value;
+    const password = passwordRef.current.value;
+
+    console.log([username, password]);
+  };
   return (
     <div className="bg-green-400 min-h-screen flex flex-col justify-center">
       <h1 className="text-center text-4xl font-bold mb-5">
@@ -12,9 +24,7 @@ const LoginPage = () => {
       <div className="max-w-sm mx-auto">
         <form
           className="w-full flex flex-col bg-white items-center rounded-2xl p-4"
-          onSubmit={(event) => {
-            event.preventDefault();
-          }}
+          onSubmit={handleSubmit}
         >
           <div className="p-3">
             <div className="p-5">
@@ -25,6 +35,7 @@ const LoginPage = () => {
                 Username
               </label>
               <input
+                ref={usernameRef}
                 className="border-0 border-b focus:bg-stone-100 focus:ring-0 focus:border-green-700 focus:text-sm focus:border-b-2 w-80"
                 id="username"
                 type="text"
@@ -41,6 +52,7 @@ const LoginPage = () => {
                 Password
               </label>
               <input
+                ref={passwordRef}
                 className="border-0 border-b focus:bg-stone-100 focus:ring-0 focus:border-green-700 focus:text-sm focus:border-b-2 w-80"
                 id="password"
                 type="password"
@@ -51,7 +63,12 @@ const LoginPage = () => {
             </div>
           </div>
           <div className="flex justify-center">
-            <button className="border border-4 border-green-700 w-40 rounded-full p-1 text-green-700 font-[700] text-lg hover:text-white hover:bg-green-700">Login</button>
+            <button
+              type="submit"
+              className="border border-4 border-green-700 w-40 rounded-full p-1 text-green-700 font-[700] text-lg hover:text-white hover:bg-green-700"
+            >
+              Login
+            </button>
           </div>
         </form>
       </div>
